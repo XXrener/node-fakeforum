c53c3e554a8e609860cfff4d14fc1c8cdb362aae
ChangHong <1287380598@qq.com>
2021-01-16 22:48:18 +0800

    更改登录数据库信息

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 module.exports = {
     apiPrefix:'/api',   //路由规范
     port:process.env.PORT||3000,    //数据库 设置
-    database:'mongodb://localhost:27017/fakeforum',
+    database:'mongodb://forum:12345@127.0.0.1:27017/fakeforum',
     databasePro:'mongodb://root:12345@服务器公务ip/端口/库名称'
 }
